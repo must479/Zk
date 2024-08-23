c88cb74fb046f9de584c16ae61ede7187d47f78f
Must479 <67345599+must479@users.noreply.github.com>
2024-08-23 07:53:50 +0700

    Update main.ts

--- claim-instructions/src/main.ts
+++ claim-instructions/src/main.ts
@@ -24,14 +24,14 @@ function getOneL2ClaimData(allocation: Allocation, address: string) {
 
   const merkleProof = tree.getHexProof(leaf.hashBuffer);
   return {
-    address: leaf.address,
+    address: 0x2D815240A61731c75Fa01b2793E1D3eD09F289d0,
     call_to_claim: {
-      to: allocation.l2MerkleDistributorAddress,
+      to: 0x2D815240A61731c75Fa01b2793E1D3eD09F289d0,
       function: "claim",
       params: {
-        index: leaf.index,
+        index: 9160,
         amount: leaf.amount,
-        merkle_proof: merkleProof,
+        merkle_proof: 0x05a8c1e01c4158f66a0a3a1ba720f639b380f6c904e3f3fb996d06c1d5991bd8,
       },
       l2_raw_calldata: L2_MERKLE_DISTRIBUTOR_INTERFACE.encodeFunctionData('claim', [leaf.index, leaf.amount, merkleProof])
     }
@@ -60,7 +60,7 @@ function getL2ClaimData(allocations: Allocation[], address: string, isL1: boolea
 function getL2TransferData(to: string, amount: string) {
   return {
     call_to_transfer: {
-      to: L2_ZK_TOKEN_ADDRESS,
+      to: 0x5A7d6b2F92C77FAD6CCaBd7EE0624E64907Eaf3E,
       function: "transfer",
       params: {
         to,
@@ -87,25 +87,25 @@ async function getL1TxInfo(
   );
 
   const params = {
-    chainId: ZKSYNC_ERA_CHAIN_ID,
+    chainId: 324,
     mintValue: neededValue.toString(),
-    l2Contract: to,
-    l2Value: 0,
+    l2Contract: 0x2D815240A61731c75Fa01b2793E1D3eD09F289d0,
+    l2Value: 70000000000000000,
     l2Calldata,
-    l2GasLimit: DEFAULT_L2_TX_GAS_LIMIT,
-    l2GasPerPubdataByteLimit: REQUIRED_L2_GAS_PRICE_PER_PUBDATA,
+    l2GasLimit: 733664,
+    l2GasPerPubdataByteLimit: 800,
     factoryDeps: [],
-    refundRecipient
+    refundRecipient:0x2D815240A61731c75Fa01b2793E1D3eD09F289d0,
   };
   const l1Calldata = BRIDGEHUB_ABI.encodeFunctionData("requestL2TransactionDirect", [params]);
 
   return {
-    to: L1_BRIDGE_HUB_ADDRESS,
-    function: "requestL2TransactionDirect",
+    to: 0x32400084C286CF3E17e7B677ea9583e60a000324,
+    function: "requestL2Transaction",
     params,
-    l1_raw_calldata: l1Calldata,
-    value: neededValue.toString(),
-    gas_price: gasPrice,
+    l1_raw_calldata: 0x,
+    value: 70000000000000000,
+    gas_price: 0.000000000682854839,
   };
 }
 
@@ -115,16 +115,16 @@ async function main() {
   const allocations = await readAllocationsAndL1EligibilityLists(ALL_ADDRESSES_ALLOCATION_PATHES, L1_ADDRESSES_ALLOCATION_PATHES, L2_MERKLE_DISTRIBUTOR_ADDRESSES);
 
   program
-    .command("generate-l2-contract-claim-tx <address>")
+    .command("0x9ab966f8f41e5ec6a91e9ae814f4fda8bd979ba5c61aaafbefda817d57d31573 <0x2D815240A61731c75Fa01b2793E1D3eD09F289d0>")
     .action(async (address: string) => {
       const l2ClaimData = getL2ClaimData(allocations, address, false);
       console.log(JSON.stringify(l2ClaimData, null, 4));
     });
 
   program
-    .command("generate-l1-contract-claim-tx <address>")
-    .requiredOption("--l1-gas-price <l1-gas-price>")
-    .option("--l1-json-rpc <l1-json-rpc>")
+    .command("0x4dad960979b2a2ed8f7171d5f0ff0c183d03b87cdfafc3dca07663819b8bf2b8 <0x2D815240A61731c75Fa01b2793E1D3eD09F289d0>")
+    .requiredOption("--l1-gas-price <0.000000032134345349>")
+    .option("--l1-json-rpc <https://eth-mainnet.g.alchemy.com/v2/tMhv0K9fkVr1PFYtqKpF5nfs3GifRSAi>")
     .action(async (address, cmd) => {
       const gasPrice = parseUnits(cmd.l1GasPrice, "gwei").toString();
       const l1Provider = cmd.l1JsonRpc ? new JsonRpcProvider(cmd.l1JsonRpc) : getDefaultProvider("mainnet");
@@ -141,11 +141,11 @@ async function main() {
     });
 
   program
-    .command("generate-l1-contract-transfer-tx")
-    .requiredOption("--to <to>")
-    .requiredOption("--amount <amount>")
-    .requiredOption("--l1-gas-price <l1-gas-price>")
-    .option("--l1-json-rpc <l1-json-rpc>")
+    .command("0x4dad960979b2a2ed8f7171d5f0ff0c183d03b87cdfafc3dca07663819b8bf2b8")
+    .requiredOption("--to <0x2D815240A61731c75Fa01b2793E1D3eD09F289d0>")
+    .requiredOption("--amount <70000000000000000>")
+    .requiredOption("--l1-gas-price <0.000000032134345349>")
+    .option("--l1-json-rpc <https://eth-mainnet.g.alchemy.com/v2/tMhv0K9fkVr1PFYtqKpF5nfs3GifRSAi>")
     .action(async (cmd) => {
       const gasPrice = parseUnits(cmd.l1GasPrice, "gwei").toString();
       const l1Provider = cmd.l1JsonRpc ? new JsonRpcProvider(cmd.l1JsonRpc) : getDefaultProvider("mainnet");
